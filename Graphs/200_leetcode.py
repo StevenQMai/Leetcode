86c7b4ebd9fd0979dddf7d762ab712265baa622b
Steven Mai <stevenmai77777@gmail.com>
2024-12-04 12:50:22 -0500

    Solved Leetcode 200 and 695 "Find number of islands" and "Find max area of islands" respectively

---
+++ Graphs/200_leetcode.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class Solution:
+    def numIslands(self, grid) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+
+        def bfs(r,c):
+            q = deque()
+            visit.add((r,c))
+            q.append((r,c))
+
+            while q:
+                row, col = q.popleft() # if you change this popleft to just pop (popping from the right), it becomes an iterative DFS
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+                for dr, dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r in range(rows) and 
+                        c in range(cols) and 
+                        grid[r][c] == "1" and 
+                        (r, c) not in visit):
+                        q.append((r, c))
+                        visit.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands += 1
+        return islands
